b5bd747f95aaf919e84e59a89fe9dc68fab3fa06
Joshua Davis <me@jdav.is>
2017-03-05 18:45:13 -0500

    added code comments

--- Arduino Uno/SimpleStrobe/SimpleStrobe.ino
+++ Arduino Uno/SimpleStrobe/SimpleStrobe.ino
@@ -1,10 +1,10 @@
-int analogPin = 0;
-int ledPin = 13;
-int onTime = 1000;
+int analogPin = 0;  // analog input pin A0
+int ledPin = 13;    // digital output pin 13 (light)
+int onTime = 1000;  // default milliseconds light is on
 
-int minDelay = 1;
-int maxDelay = 1000;
-int strobeDelay = 0;
+int minDelay = 1;     // min delay time (range)
+int maxDelay = 1000;  // max delay time (range)
+int strobeDelay = 0;  // calculated actual delay (between min and max)
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,7 +13,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  strobeDelay = map(analogRead(analogPin), 0, 1023, minDelay, maxDelay);
+  strobeDelay = map(analogRead(analogPin), 0, 1023, minDelay, maxDelay);  // get value of potentiometer and calculate delay
 
   digitalWrite(ledPin, HIGH);
   delayMicroseconds(onTime);
